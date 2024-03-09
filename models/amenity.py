978c8b0c414c6bdecfe803716fdb842ced7dc708
badr-elmasry <badr.ck.elmasry@gmail.com>
2024-03-09 11:39:20 -0800

    push

--- models/amenity.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-""" shebang line - defines where the interpreter is located """
-from models.base_model import BaseModel
-""" import moduls """
-
-
-class Amenity(BaseModel):
-    """ Main class to manage Amenities, it inherited of BaseModel """
-    name = ""
